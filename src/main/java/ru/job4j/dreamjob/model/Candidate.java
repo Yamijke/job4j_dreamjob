868f4130c1a688ebd58407796ba7062b2f2df168
Yamijke <serhei.knyazev@gmail.com>
2024-03-06 16:28:12 +0300

    5. Список кандидатов [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -0,0 +1,68 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Candidate {
+    private int id;
+    private String name;
+    private String level;
+    private LocalDateTime creationDate;
+
+
+    public Candidate(int id, String name, String level, LocalDateTime creationDate) {
+        this.id = id;
+        this.name = name;
+        this.level = level;
+        this.creationDate = creationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id && Objects.equals(name, candidate.name) && Objects.equals(level, candidate.level) && Objects.equals(creationDate, candidate.creationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, level, creationDate);
+    }
+}
